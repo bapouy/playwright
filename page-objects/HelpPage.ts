5cdf27c48aff6f68e9f9e85e49e745276c0ef5a0
Daniel Norman <bapo@pop-os.local>
2025-11-02 13:17:11 +0200

    added functions for helpPage

--- page-objects/HelpPage.ts
+++ page-objects/HelpPage.ts
@@ -1,8 +1,10 @@
+import { expect, Page } from "@playwright/test";
+
 export enum HelpSelectors {
   // Search
   HELP_SEARCH_INPUT = "#helpsearch",
   HELP_RESULT_STATS = "#help-result-stats",
-  
+
   // Feedback
   FEEDBACK_RADIO_CONFUSED = 'input[type="radio"][name="info"][value="Confused By Information"]',
   FEEDBACK_THANKS_MESSAGE = "#hmd-ConfirmNoBox",
@@ -16,11 +18,14 @@ export enum HelpTexts {
   YOUR_ORDERS_TRACK = "Your Orders Track or cancel",
   CONTACT_CARRIER = "contact a carrier",
   MISSING_TRACKING = "Missing Tracking Information",
-  
+  SIGN_IN = "Amazon Sign-In",
+
   // Content
   ARRIVING = "Arriving",
   RECOMMENDED_TOPICS = "Recommended Help Topics",
-  
+  CONTACT_CARRIER_TITLE = "Amazon Carrier Contact Information - Amazon Customer Service",
+  FEEBACK_RESPONSE = "Thanks! While we're unable to respond directly to your feedback, we'll use this information to improve our online Help.",
+
   // Buttons
   FEEDBACK_NO = "No",
   FEEDBACK_YES = "Yes",
@@ -33,3 +38,170 @@ export enum HelpRoles {
   BUTTON = "button",
 }
 
+//looks like dynamically generated, but ther is not other option, useage of classes is not recomended
+export enum HelpURLs {
+  TRACKING_PACKAGE = "https://www.amazon.com/gp/help/customer/display.html?nodeId=GENAFPTNLHV7ZACW#GUID-AEBE3FF9-5AAF-4CD6-AF09-0287157B72C3__GUID-3347AA8A-20A8-497F-8B2E-C946905E8963",
+}
+
+// Regex pattern for search results
+export const SEARCH_RESULT_REGEX = /\d+\s+search\s+results\s+for/;
+
+export async function validateOrdersLink(page: Page) {
+  // Locate the link that directs to the customer's order history.
+  const ordersLink = await page
+    .locator("a", { hasText: HelpTexts.YOUR_ORDERS })
+    .first(); //Thre is an ID, but looks like dynamically generated so it may break on the next deployment despite it is not a best practice to use it
+
+  await expect(ordersLink).toBeVisible();
+  await expect(ordersLink).toHaveAttribute("href", /.*order-history.*/);
+
+  const [newPage] = await Promise.all([
+    page.waitForEvent("popup"),
+    ordersLink.click(),
+  ]);
+  await expect(newPage.url()).toContain("signin"); // Usually I would login by API in the beforeEach, and check the propper result, do not want to depend on UI login to avoid flakiness
+  await newPage.close();
+}
+
+export async function validateJumpLinks(page: Page) {
+  const jumpLink = await page.locator("a", {
+    hasText: HelpTexts.TRACKING_PACKAGE,
+  });
+  await expect(jumpLink).toBeVisible();
+  await jumpLink.click();
+  await expect(page).toHaveURL(HelpURLs.TRACKING_PACKAGE); //it may break on next deploymnet, looks like dynmically generated, but ther is not other option, useage of classes is not recomended
+}
+
+export async function validateSidebarLinks(page: Page) {
+  const sidebarLink = await page
+    .getByRole(HelpRoles.LISTITEM)
+    .filter({ hasText: HelpTexts.RETURNS_REFUNDS });
+
+  await expect(sidebarLink).toBeVisible();
+}
+
+export async function clickSidebarLink(page: Page) {
+  await page
+    .getByRole(HelpRoles.LISTITEM)
+    .filter({ hasText: HelpTexts.RETURNS_REFUNDS })
+    .click();
+}
+
+export async function validateSignInPageTitle(page: Page) {
+  await expect(page).toHaveTitle(HelpTexts.SIGN_IN);
+}
+
+export async function performSearch(page: Page, query: string) {
+  const searchInput = await page.locator(HelpSelectors.HELP_SEARCH_INPUT);
+  await expect(searchInput).toBeVisible();
+  await searchInput.fill(query);
+  await page.keyboard.press("Enter");
+}
+
+export async function validateSearchStatus(page: Page) {
+  await expect(page.locator(HelpSelectors.HELP_RESULT_STATS)).toContainText(
+    SEARCH_RESULT_REGEX
+  );
+}
+
+export async function validateArrivingStatusDefinition(page: Page) {
+  const shippedDefinition = await page.getByText(HelpTexts.ARRIVING);
+  await expect(shippedDefinition).toBeVisible();
+  await expect(shippedDefinition).toContainText(HelpTexts.ARRIVING); // I know this is a usless test, but we have only a dynamically generated ID
+}
+
+export async function validateContactLink(page: Page) {
+  const contactLink = await page.getByRole(HelpRoles.LINK, {
+    name: HelpTexts.CONTACT_CARRIER,
+    exact: true,
+  });
+
+  await expect(contactLink).toBeVisible();
+}
+
+export async function clickContactLink(page: Page) {
+  const contactLink = await page.getByRole(HelpRoles.LINK, {
+    name: HelpTexts.CONTACT_CARRIER,
+    exact: true,
+  });
+
+  await contactLink.click();
+  await expect(page).toHaveTitle(HelpTexts.CONTACT_CARRIER_TITLE);
+}
+
+export async function validateMobileView(page: Page) {
+  await page.setViewportSize({ width: 375, height: 812 });
+
+  // Functional check: Ensure the left navigation disappears or collapses
+  const leftNav = await page
+    .getByRole(HelpRoles.LISTITEM)
+    .filter({ hasText: HelpTexts.YOUR_ORDERS_TRACK });
+  // await expect(leftNav).not.toBeVisible(); not good responsiveness, there are no colapsing or disappearing elements
+
+  await expect(leftNav).toBeVisible();
+}
+
+export async function validateFeedback(page: Page) {
+  const feedbackNoButton = await page.getByRole(HelpRoles.BUTTON, {
+    name: HelpTexts.FEEDBACK_NO,
+  });
+  const feedbackYesButton = await page.getByRole(HelpRoles.BUTTON, {
+    name: HelpTexts.FEEDBACK_YES,
+  });
+
+  await expect(feedbackNoButton).toBeVisible();
+  await expect(feedbackYesButton).toBeVisible();
+}
+
+export async function performFeedback(page: Page, yesNo: boolean) {
+  const feedbackNoButton = await page.getByRole(HelpRoles.BUTTON, {
+    name: HelpTexts.FEEDBACK_NO,
+  });
+  const feedbackYesButton = await page.getByRole(HelpRoles.BUTTON, {
+    name: HelpTexts.FEEDBACK_YES,
+  });
+
+  if (yesNo) {
+    await feedbackYesButton.click();
+
+    await page.click(HelpSelectors.FEEDBACK_RADIO_CONFUSED);
+    await page
+      .getByRole(HelpRoles.BUTTON, { name: HelpTexts.FEEDBACK_SUBMIT })
+      .last()
+      .click();
+
+    const feedbackTahanks = await page.locator(
+      HelpSelectors.FEEDBACK_THANKS_MESSAGE
+    );
+    await expect(feedbackTahanks).toBeVisible();
+    await expect(feedbackTahanks).toHaveText(HelpTexts.FEEBACK_RESPONSE);
+  } else {
+    await feedbackNoButton.click();
+
+    await page.click(HelpSelectors.FEEDBACK_RADIO_CONFUSED);
+    await page
+      .getByRole(HelpRoles.BUTTON, { name: HelpTexts.FEEDBACK_SUBMIT })
+      .last()
+      .click();
+
+    const feedbackTahanks = await page.locator(
+      HelpSelectors.FEEDBACK_THANKS_MESSAGE
+    );
+    await expect(feedbackTahanks).toBeVisible();
+    await expect(feedbackTahanks).toHaveText(HelpTexts.FEEBACK_RESPONSE);
+  }
+}
+
+export async function validateRecommendedTopics(page: Page) {
+  const recommendedTopicsList = await page.getByText(
+    HelpTexts.RECOMMENDED_TOPICS
+  );
+
+  await expect(recommendedTopicsList).toBeVisible();
+
+  const missingTrackingLink = await page
+    .getByRole(HelpRoles.LINK, { name: HelpTexts.MISSING_TRACKING })
+    .first();
+
+  await expect(missingTrackingLink).toBeVisible();
+}
--- tests/help.test.ts
+++ tests/help.test.ts
@@ -1,5 +1,20 @@
 import { test, expect } from "@playwright/test";
-import { HelpSelectors, HelpTexts, HelpRoles } from "../page-objects/help";
+import {
+  validateOrdersLink,
+  validateJumpLinks,
+  validateSidebarLinks,
+  clickSidebarLink,
+  validateSignInPageTitle,
+  performSearch,
+  validateSearchStatus,
+  validateArrivingStatusDefinition,
+  clickContactLink,
+  validateContactLink,
+  validateMobileView,
+  performFeedback,
+  validateFeedback,
+  validateRecommendedTopics,
+} from "../page-objects/HelpPage";
 
 // Placeholder for the URL. You will replace this with the actual Amazon Help URL.
 const HELP_PAGE_URL =
@@ -12,78 +27,33 @@ test.describe("Test cases for 1.3", () => {
   });
 
   test('Verify external link to "Your Orders"', async ({ page }) => {
-    // Locate the link that directs to the customer's order history.
-    const ordersLink = await page
-      .locator("a", { hasText: HelpTexts.YOUR_ORDERS })
-      .first(); //Thre is an ID, but looks like dynamically generated so it may break on the next deployment despite it is not a best practice to use it
-
-    await expect(ordersLink).toBeVisible();
-    await expect(ordersLink).toHaveAttribute("href", /.*order-history.*/);
-
-    const [newPage] = await Promise.all([
-      page.waitForEvent("popup"),
-      ordersLink.click(),
-    ]);
-    await expect(newPage.url()).toContain("signin"); // Usually I would login by API in the beforeEach, and check the propper result, do not want to depend on UI login to avoid flakiness
-    await newPage.close();
+    await validateOrdersLink(page);
   });
 
   test("Verify jump links (Table of Contents)", async ({ page }) => {
-    const jumpLink = await page.locator("a", {
-      hasText: HelpTexts.TRACKING_PACKAGE,
-    });
-    await expect(jumpLink).toBeVisible();
-    await jumpLink.click();
-    await expect(page).toHaveURL(
-      /#GUID-AEBE3FF9-5AAF-4CD6-AF09-0287157B72C3__GUID-3347AA8A-20A8-497F-8B2E-C946905E8963/
-    ); //it may break on next deploymnet, looks like dynmically generated, but ther is not other option, useage of classes is not recomended
+    await validateJumpLinks(page);
   });
 
   test("Verify navigational sidebar links", async ({ page }) => {
-    const sidebarLink = await page
-      .getByRole(HelpRoles.LISTITEM)
-      .filter({ hasText: HelpTexts.RETURNS_REFUNDS });
-
-    await expect(sidebarLink).toBeVisible();
-
-    await sidebarLink.click();
-    await expect(page).toHaveTitle("Amazon Sign-In"); // Usually I would login by API in the beforeEach, and check the propper result, do not want to depend on UI login to avoid flakiness
+    await validateSidebarLinks(page);
+    await clickSidebarLink(page);
+    await validateSignInPageTitle(page); // Usually I would login by API in the beforeEach, and check the propper result, do not want to depend on UI login to avoid flakiness
   });
 
   test("Verify search bar functionality", async ({ page }) => {
-    const searchInput = await page.locator(HelpSelectors.HELP_SEARCH_INPUT);
-
-    const testQuery = "contact amazon";
-
-    await expect(searchInput).toBeVisible();
-    await searchInput.fill(testQuery);
-    await page.keyboard.press("Enter");
-
-    await expect(page.locator(HelpSelectors.HELP_RESULT_STATS)).toContainText(
-      /\d+\s+search\s+results\s+for/ //s due to extra white spaces
-    );
+    await performSearch(page, "contact amazon");
+    await validateSearchStatus(page);
   });
 
-  test("CSC-002: Verify accuracy of Delivery Status definition (Shipped)", async ({
+  test("CSC-002: Verify accuracy of Arriving Status definition", async ({
     page,
   }) => {
-    const shippedDefinition = await page.getByText(HelpTexts.ARRIVING);
-    await expect(shippedDefinition).toBeVisible();
-    await expect(shippedDefinition).toContainText(HelpTexts.ARRIVING); // I know this is a usless test, but we have only a dynamically generated ID
+    await validateArrivingStatusDefinition(page);
   });
 
   test("CSC-004: Verify link to external contact options", async ({ page }) => {
-    const contactLink = await page.getByRole(HelpRoles.LINK, {
-      name: HelpTexts.CONTACT_CARRIER,
-      exact: true,
-    });
-
-    await expect(contactLink).toBeVisible();
-
-    await contactLink.click();
-    await expect(page).toHaveTitle(
-      "Amazon Carrier Contact Information - Amazon Customer Service"
-    );
+    await validateContactLink(page);
+    await clickContactLink(page);
   });
 
   // --- 4.3 Usability and Experience Test Cases (UX) ---
@@ -91,48 +61,17 @@ test.describe("Test cases for 1.3", () => {
   test("UXC-001: Verify mobile responsiveness (Snapshot Test)", async ({
     page,
   }) => {
-    await page.setViewportSize({ width: 375, height: 812 });
-
-    // Functional check: Ensure the left navigation disappears or collapses
-    const leftNav = await page
-      .getByRole(HelpRoles.LISTITEM)
-      .filter({ hasText: HelpTexts.YOUR_ORDERS_TRACK });
-    // await expect(leftNav).not.toBeVisible(); not good responsiveness, there are no colapsing or disappearing elements
-
-    await expect(leftNav).toBeVisible();
+    await validateMobileView(page);
   });
 
   test("UXC-003: Verify the Feedback mechanism (Click No)", async ({
     page,
   }) => {
-    const feedbackNoButton = await page.getByRole(HelpRoles.BUTTON, { name: HelpTexts.FEEDBACK_NO });
-    const feedbackYesButton = await page.getByRole(HelpRoles.BUTTON, { name: HelpTexts.FEEDBACK_YES });
-
-    await expect(feedbackNoButton).toBeVisible();
-    await expect(feedbackYesButton).toBeVisible();
-    await feedbackNoButton.click();
-
-    await page.click(HelpSelectors.FEEDBACK_RADIO_CONFUSED);
-    await page.getByRole(HelpRoles.BUTTON, { name: HelpTexts.FEEDBACK_SUBMIT }).last().click();
-
-    const feedbackTahanks = await page.locator(HelpSelectors.FEEDBACK_THANKS_MESSAGE);
-    await expect(feedbackTahanks).toBeVisible();
-    await expect(feedbackTahanks).toHaveText(
-      "Thanks! While we're unable to respond directly to your feedback, we'll use this information to improve our online Help."
-    );
+    await validateFeedback(page);
+    await performFeedback(page, false);
   });
 
   test("UXC-004: Verify Recommended Help Topics", async ({ page }) => {
-    const recommendedTopicsList = await page.getByText(
-      HelpTexts.RECOMMENDED_TOPICS
-    );
-
-    await expect(recommendedTopicsList).toBeVisible();
-
-    const missingTrackingLink = await page
-      .getByRole(HelpRoles.LINK, { name: HelpTexts.MISSING_TRACKING })
-      .first();
-
-    await expect(missingTrackingLink).toBeVisible();
+    await validateRecommendedTopics(page);
   });
 });
